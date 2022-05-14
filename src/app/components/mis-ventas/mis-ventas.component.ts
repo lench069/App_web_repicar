78c7f91d29195de00daeb5a8778eb7634fa147d3
Lenin Velastegui <lcvelastegui@gmail.com>
2022-05-14 15:26:26 -0500

    REPIxxx se agrega libreria para crear pdfs

--- src/app/components/mis-ventas/mis-ventas.component.ts
+++ src/app/components/mis-ventas/mis-ventas.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { ServiciosService } from 'src/app/services/servicios.service';
 import { ConfiguracionesComponent } from '../PageAdministradores/configuraciones/configuraciones.component';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-mis-ventas',
@@ -156,9 +159,6 @@ export class MisVentasComponent implements OnInit {
         this.recargo_total = this.total_ventas.TOTAL_ORIGINAL_COM - this.total_ventas.TOTAL_ORIGINAL;
         console.log(this.selectPublicidad);
         this.precio_total = parseFloat(this.selectPublicidad.PRECIO) + parseFloat(this.selectLicencia.VALOR) + this.recargo_total;
-        console.log(this.cantidad_total);
-        console.log(this.recargo_total);
-        console.log(this.precio_total);
       } else {
         this.toastr.warning('No existen licencias!', 'Aviso');
       }
@@ -211,6 +211,33 @@ export class MisVentasComponent implements OnInit {
  
     });
    
+  }
+
+  createPDF(){
+ 
+    const pdfDefinition: any = {
+      content: [
+        {
+          layout: 'lightHorizontalLines', // optional
+          table: {
+            // headers are automatically repeated if the table spans over multiple pages
+            // you can declare how many rows should be treated as headers
+            headerRows: 1,
+            widths: [ '*', 'auto', 100, '*' ],
+    
+            body: [
+              [ 'First', 'Second', 'Third', 'The last one' ],
+              [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+              [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+            ]
+          }
+        }
+      ]
+    }
+ 
+    const pdf = pdfMake.createPdf(pdfDefinition);
+    pdf.open();
+ 
   }
 
 }
